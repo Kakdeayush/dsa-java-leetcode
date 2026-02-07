7bc961c080674ac4fe955bf001f3431cf459f02e
Kakdeayush <kakdeayush376@gmail.com>
2026-02-07 10:45:34 +0530

    container with most water problem solved

---
+++ arrays/Container_with_most_water.java
@@ -0,0 +1,22 @@
+public class Container_with_most_water {
+    public int maxArea(int[] height) {
+
+        int max_area=0;
+        int start=0;
+        int end=height.length-1;
+
+        while(start<end){
+            if(height[start]<height[end]){
+                max_area=Math.max(max_area,height[start]*(end-start));
+                start ++;
+
+            }else{
+                 max_area=Math.max(max_area,height[end]*(end-start));
+                 end--;
+            }
+        }
+
+        return max_area;
+        
+    }
+}
